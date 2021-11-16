ad0734e8e38faa5af5fe9e3f299e28b3eda2cce5
abiha-aftab <42846013+abiha-aftab@users.noreply.github.com>
2021-11-17 01:27:52 +0500

    Update index.js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,17 +4,8 @@ import FooterDefault from '../../containers/Footer/variants/FooterDefault'
 import NavbarDefault from '../../containers/Navbar/variants/NavbarDefault'
 import { useStaticQuery, graphql } from 'gatsby'
 import { prepareDataLinks } from '../../utils/prepareDataLinks'
-import KontentSmartLink, { KontentSmartLinkEvent } from '@kentico/kontent-smart-link';
+import KontentSmartLink from '@kentico/kontent-smart-link';
 
-const sdk = KontentSmartLink.initialize({ ... });
-sdk.on(
-  KontentSmartLinkEvent.Refresh,
-  (data, metadata, originalRefresh) => {
-    // Custom refresh logic goes here
-    // ...
-    originalRefresh(); // If you want to refresh the page at the end
-  }
-); 
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`

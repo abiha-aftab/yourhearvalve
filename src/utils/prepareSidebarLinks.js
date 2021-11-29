ac2a881a7bdf3c242b263bd989818c44751fa1f0
Abiha Aftab <aaftab@xavor.com>
2021-11-29 16:29:47 +0500

    files changed

--- src/utils/prepareSidebarLinks.js
+++ src/utils/prepareSidebarLinks.js
@@ -1,10 +1,18 @@
 export const prepareSidebarLinks = (data) => {
+  console.log(data)
+  let abc = 'sidebar here'
+  !data ? (data = 'sidebar here') : (data = data)
+
   let sidebarLinks = []
+  if (data === 'sidebar here') return sidebarLinks
   let links = []
   let baseURL = ''
-  const { title: {value: name} } = data.elements
-  if(data.elements.menu_items) {
-    links = data.elements.menu_items.value.forEach(menu => {
+  console.log('datanow', data)
+  const {
+    title: { value: name },
+  } = data.elements
+  if (data.elements.menu_items) {
+    links = data.elements.menu_items.value.forEach((menu) => {
       links = menu.elements.links.value
       baseURL = menu.elements.slug.value
       sidebarLinks.push({
@@ -21,14 +29,24 @@ export const prepareSidebarLinks = (data) => {
   return sidebarLinks
 }
 const prepareLinks = (baseURL, links, sidebarLinks) => {
-  links.forEach(link => {
-    const { title: {value: name} } = link.elements
-    let slug = link.elements.url ? link.elements.url.value : link.elements.slug.value
-    let url = slug ? slug.indexOf("https") !== -1 ? slug : baseURL === slug ? `/${slug}` : `/${baseURL}/${slug}` : ""
-    if(baseURL === slug) return
+  links.forEach((link) => {
+    const {
+      title: { value: name },
+    } = link.elements
+    let slug = link.elements.url
+      ? link.elements.url.value
+      : link.elements.slug.value
+    let url = slug
+      ? slug.indexOf('https') !== -1
+        ? slug
+        : baseURL === slug
+        ? `/${slug}`
+        : `/${baseURL}/${slug}`
+      : ''
+    if (baseURL === slug) return
     sidebarLinks.push({
       name: name,
-      url: url
+      url: url,
     })
   })
   return sidebarLinks
